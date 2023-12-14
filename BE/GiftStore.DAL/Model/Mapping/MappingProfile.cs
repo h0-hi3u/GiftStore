a7d5956704d62e0f8db0c21a5ac3cefbf573eb57
h0-hi3u <hieuenglishabc@gmail.com>
2023-12-14 17:58:48 +0700

    add mapping profile for bestSeller, ImagaProduct, User

--- BE/GiftStore.DAL/Model/Mapping/MappingProfile.cs
+++ BE/GiftStore.DAL/Model/Mapping/MappingProfile.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using GiftStore.DAL.Model.Dto.BestSeller;
 using GiftStore.DAL.Model.Dto.Category;
 using GiftStore.DAL.Model.Dto.Collection;
+using GiftStore.DAL.Model.Dto.ImageProduct;
 using GiftStore.DAL.Model.Dto.Supplier;
 using GiftStore.DAL.Model.Dto.Tag;
+using GiftStore.DAL.Model.Dto.User;
 using GiftStore.DAL.Model.Entity;
 
 namespace GiftStore.DAL.Model.Mapping;
@@ -45,5 +48,25 @@ public class MappingProfile : Profile
         CreateMap<Collection, CollectionShowResponseDto>();
         CreateMap<CollectionCreateRequestDto, Collection>();
         CreateMap<CollectionUpdateRequestDto, Collection>();
+    }
+
+    public void BestSellerMappingProfile()
+    {
+        CreateMap<BestSeller, BestSellerShowResponseDto>();
+        CreateMap<BestSellerCreateRequestDto, BestSeller>();
+    }
+
+    public void ImageProductMappingProfile()
+    {
+        CreateMap<ImageProduct, ImageProductShowResponseDto>();
+        CreateMap<ImageProductCreateRequestDto, ImageProduct>();
+        CreateMap<ImageProductUpdateRequestDto, ImageProduct>();
+    }
+
+    public void UserMappingProfile()
+    {
+        CreateMap<User, UserShowResponseDto>();
+        CreateMap<UserUpdateRequestDto, User>();
+        CreateMap<UserCreateRequestDto, User>();
     }
 }
