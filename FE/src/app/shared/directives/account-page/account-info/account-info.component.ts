055d89cca439727d322918500059f399b6a787f2
h0-hi3u <hieuenglishabc@gmail.com>
2024-01-12 23:09:09 +0700

    css for account-page, account-order, account-info

--- FE/src/app/shared/directives/account-page/account-info/account-info.component.ts
+++ FE/src/app/shared/directives/account-page/account-info/account-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account-info',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-info.component.scss']
 })
 export class AccountInfoComponent {
+  // @Input() name!: string;
+  // @Input() email!: string;
 
 }
--- FE/src/app/shared/directives/account-page/account-page.component.ts
+++ FE/src/app/shared/directives/account-page/account-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.scss']
 })
-export class AccountPageComponent {
+export class AccountPageComponent implements OnInit {
+  urlString: string = "";
+  arrUrl: string[] = [];
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    //this.loadUrl();
+    this.urlString = 'account';
+  }
+  // public loadUrl() {
+  //   this.arrUrl = this.router.url.split("/");
+  //   this.urlString = this.arrUrl[this.arrUrl.length - 1];
+  //   console.log(this.urlString);
+  // }
+  public goToInfo() : void {
+    this.router.navigate(['account']);
+    this.urlString = 'account';
+  }
+  public goToOrders() : void {
+    this.router.navigate(['account/orders']);
+    this.urlString = 'orders';
+  }
+  public goToChangePassword() : void {
+    this.router.navigate(['account/change-password']);
+    this.urlString = 'change-password';
+  }
+  public goToAddresses(): void {
+    this.router.navigate(['account/addresses']);
+    this.urlString = 'addresses';
+  }
 }
