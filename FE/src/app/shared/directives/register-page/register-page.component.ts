95acaf8055cdc1b94482b4d45a8327c493033423
h0-hi3u <hieuenglishabc@gmail.com>
2024-01-14 21:55:57 +0700

    validate form in register-page

--- FE/src/app/shared/directives/register-page/register-page.component.ts
+++ FE/src/app/shared/directives/register-page/register-page.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,8 +9,35 @@ import { Router } from '@angular/router';
 })
 export class RegisterPageComponent {
 
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+  registerForm = this.formBuilder.group({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.minLength(10), Validators.maxLength(10), Validators.required, Validators.pattern('^0\d[0-9]$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  }); 
+  get getForm() {
+    return this.registerForm.controls;
+  }
+  public register() {
+    console.log(this.registerForm.controls.phoneNumber.errors);
+    
+    if (this.registerForm.valid) {
+      // const user: UserLoginDto = { email: this.getForm.email.value || '', password: this.getForm.password.value || ''};
+      // this.authService.login(user).subscribe((res: ResponseDto) => {
+      //   if(res.isSuccess) {
+      //     localStorage.setItem("access_token", res.data);
+      //     this.router.navigate(['']);
+      //   } else {
+      //     this.errLogin = res.detail;
+      //     this.isError = true;
+      //   }
+      // });
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
   public goToLogin(): void {
     this.router.navigate(['login']);
   }
