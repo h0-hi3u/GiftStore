14c96d03607407de4e1b71ff37ab0e4c433be731
h0-hi3u <hieuenglishabc@gmail.com>
2024-04-16 22:57:11 +0700

    login and store image firebase 90%

---
+++ FE/src/app/core/models/ImageProduct/imageProductCreateDto.ts
@@ -0,0 +1,3 @@
+export interface ImageProductCreateDto {
+    image: ImageBitmap,
+}
--- FE/src/environment/environment.ts
+++ FE/src/environment/environment.ts
@@ -1,12 +1,12 @@
 export const environment =  {
     urlApi : 'https://localhost:7196/api',
     firebaseConfig : {
-        apiKey: "AIzaSyDPvidxIxkDuH7K4ivBSdcd2KZ-4eOd3-E",
-        authDomain: "giftstore-d8398.firebaseapp.com",
-        projectId: "giftstore-d8398",
-        storageBucket: "giftstore-d8398.appspot.com",
-        messagingSenderId: "1073531347051",
-        appId: "1:1073531347051:web:f62b11f47904ad8143512e",
-        measurementId: "G-K8V0NB3KXQ"
+        apiKey: "AIzaSyDwV5Z9BYedOYcRPfu-gGtd0OcB4XSxwxI",
+        authDomain: "giftstore-416213.firebaseapp.com",
+        projectId: "giftstore-416213",
+        storageBucket: "giftstore-416213.appspot.com",
+        messagingSenderId: "356858350916",
+        appId: "1:356858350916:web:927996fc745507f2b0db11",
+        measurementId: "G-YGTSHJ7WFP"
     }
 }
